2f1a8f22f75d404ffc6f4f3cba3c388f5f36e3dd
quallsma <quallsma@msu.edu>
2017-06-28 18:08:30 -0400

    Wrap up for 06/28/2016

--- ClosestToZeroService.Test/UnitTest1.cs
+++ ClosestToZeroService.Test/UnitTest1.cs
@@ -1,14 +1,59 @@
 using System;
 using Xunit;
+using ClosestToZeroService;
 
 namespace ClosestToZeroService.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public void TestShouldThrowExceptionForNoValue() {
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Throws<ArgumentException>(() => closestToZero.GetClosest(null));
         }
+
+        [Fact]
+        public void TestShouldThrowExceptionforAnEmptyArray() {
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Throws<ArgumentException>(() => closestToZero.GetClosest(new int[0]));
+        }
+
+        [Fact]
+        public void FunctionGetClosestGivenArrayWithOneElementReturnsOneElement(){
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Equal(3, closestToZero.GetClosest(new int[1] {3}));
+        }
+
+        [Fact]
+        public void FunctionGetClosestGivenArrayWithTwoPositiveElementsReturnsClosetToZero(){
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Equal(4, closestToZero.GetClosest(new int[2] {4, 5}));
+        }
+
+        [Fact]
+        public void FunctionGetClosestGivenArrayWithThreePositiveElementsReturnsClosestToZero(){
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Equal(3, closestToZero.GetClosest(new int[3] {4, 3, 5}));
+        }
+
+        [Fact]
+        public void FunctionGetClosestGivenArrayWithThreePositiveElementsContainingZeroReturnsZero() {
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Equal(0, closestToZero.GetClosest(new int[3] {7, 0, 8}));
+        }
+
+        [Fact]
+        public void FunctionGetClosestGivenArrayWithTwoNegativeElementsReturnsTheElementClosestToZero() {
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Equal(-1, closestToZero.GetClosest(new int[2] {-4, -1}));
+        }
+
+        [Fact]
+        public void FunctionGetClosestGivenArrayWithOneNegativeElementAndOnePositiveElementReturnsTheElementClosestToZero() {
+            ClosestToZero closestToZero = new ClosestToZero();
+            Assert.Equal(2, closestToZero.GetClosest(new int[2] {-2, 2}));
+        }
+
     }
 }
--- ClosestToZeroService/ClosestToZero.cs
+++ ClosestToZeroService/ClosestToZero.cs
@@ -3,7 +3,22 @@
 namespace ClosestToZeroService
 {
     public class ClosestToZero
+    
     {
-        
+        public int GetClosest(int[] InputArray){
+            if (InputArray == null || InputArray.Length == 0) {
+                throw new ArgumentException();
+            } 
+            int closest = int.MinValue;
+            foreach (int element in InputArray){
+                if (closest == int.MinValue || Math.Abs(element) < Math.Abs(closest)) {
+                    closest = element;
+                } 
+               if(Math.Abs(closest) == Math.Abs(element)){
+                    closest = Math.Abs(element);
+                }
+            }
+            return closest;
+        }
     }
 }
